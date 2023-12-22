5d6d3fdfac457a91db4c67d3ccf6e5167d4e221e
AndreyPolacow <60852374+AndreyPolacow@users.noreply.github.com>
2023-12-22 21:34:50 +0300

    Update AuthController.java

--- src/main/java/com/spring_recipe/demo/api/rest/AuthController.java
+++ src/main/java/com/spring_recipe/demo/api/rest/AuthController.java
@@ -1,9 +1,9 @@
 package com.spring_recipe.demo.api.rest;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/auth")
@@ -18,15 +18,4 @@ public class AuthController {
     public String getSuccessPage() {
         return "success";
     }
-
-  @PostMapping("/success")
-    public String verifyCaptcha(HttpServletRequest request, @RequestParam("captcha") String captcha) {
-        String captchaSession = (String) request.getSession().getAttribute("captcha");
-        if (captcha.equals(captchaSession)) {
-            return "success";
-        } else {
-            return "captcha-failure";
-        }
-    }
-
 }
